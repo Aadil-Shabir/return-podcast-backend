fe2c9981daf77a2d6e57b42f0a27c2a97b57cca9
Aadil-Shabir <aadil.shabir13@gmail.com>
2025-10-24 22:37:43 +0500

    minor change

--- middleware/corsHandler.js
+++ middleware/corsHandler.js
@@ -2,7 +2,12 @@
 const cors = require("cors");
 
 const corsOptions = {
-  origin: ["http://localhost:5173", "https://returnus.com"],
+  origin: [
+    "http://localhost:5173",
+    "https://returnus.com",
+    ,
+    "http://returnus.com",
+  ],
   credentials: true,
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
   allowedHeaders: ["Content-Type", "Authorization", "X-Requested-With"],
